f94f3383efd0c5ef28c1317d7d649a189516a4fd
Jayascript <6750308-jayascript@users.noreply.gitlab.com>
2020-12-04 20:16:25 -0500

    Refactor gameplay code

--- solitaire.py
+++ solitaire.py
@@ -6,18 +6,21 @@ import numpy as np
 from strategy import Game
 
 
-pp = pprint.PrettyPrinter(indent = 4)
-warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning) 
+pp = pprint.PrettyPrinter(indent=4)
+warnings.filterwarnings("ignore", category=np.VisibleDeprecationWarning)
 
 
 def get_user_input():
-    pay_to_play = input("\nHow much would you like to bet? Input an amount with numbers and\ndecimals only (e.g. 1.50).\n >> ")
+    pay_to_play = input(
+        "\nHow much would you like to bet? Input an amount with numbers and\ndecimals"\
+        "only (e.g. 1.50).\n >> "
+    )
     try:
         buyin_fee = float(pay_to_play)
     except:
         print("Unknown input. Using default value: $1.50")
         buyin_fee = 1.50
-        
+
     see_each_move = input("\nWould you like to see each move? Y/N:\n >> ")
     if see_each_move == "Y":
         verbose = True
@@ -26,7 +29,7 @@ def get_user_input():
     else:
         print("Unknown input. Using default value: verbose = False")
         verbose = False
-        
+
     simple_or_strategic = input("\nChoose a mode:\n1) Strategic\n2) Simple\n >> ")
     if simple_or_strategic == "1":
         turn = "strategic"
@@ -35,8 +38,9 @@ def get_user_input():
     else:
         print("Unknown input. Using default value: strategic")
         turn = "strategic"
-    
+
     return buyin_fee, verbose, turn
+
 
 def main():
     print("--- STRATEGIC SOLITAIRE ---")
@@ -48,7 +52,7 @@ def main():
     place_amount = buyin_fee * 0.3
     bonus_amount = buyin_fee * 3
 
-    seed = 84 # Set to None for random state; 84 for win state
+    seed = 84  # Set to None for random state; 84 for win state
     random.seed(seed)
     verbose = output
     turn = mode
@@ -56,13 +60,16 @@ def main():
     play = Game()
     play.simulate(turn=turn, verbose=verbose)
 
-    if(play.win()):
+    if play.win():
         result = 1
-        payout += (52*place_amount) + bonus_amount
+        payout += (52 * place_amount) + bonus_amount
         print(f"\nYou won! Total winnings: {payout - buyin_fee}\n")
     else:
         result = 0
-        placed = [np.sum([1 for card in play.ace_stacks[i].cards]) for i in list(play.ace_stacks.keys())]
+        placed = [
+            np.sum([1 for card in play.ace_stacks[i].cards])
+            for i in list(play.ace_stacks.keys())
+        ]
         payout += np.sum(placed) * place_amount
         print(f"\nYou lost! Try again. Total winnings: {payout - buyin_fee}\n")
 
